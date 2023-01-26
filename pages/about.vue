167235691050c5c6959f924f0d73d8b6a19b56d1
Brian <bbarr0101@gmail.com>
2023-01-25 17:58:43 -0800

    finished about page banner and document body (tentatively)

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,27 @@
-<template>
-    <div>
-        <h2>about</h2>
+<template >
+    <div id="about-banner" class="bg-center bg-cover bg-no-repeat py-64
+    bg-[url('https://upload.wikimedia.org/wikipedia/commons/a/a5/Into_the_Jaws_of_Death_23-0455M_edit.jpg')]">
     </div>
+    <main class=" my-12 container mx-auto ">
+        <h2 class="mt-4 text-center text-6xl text-black/75">About the Battles of WWII</h2>
+        <p class="my-6 mx-16 text-lg">
+            Battles of WWII is a site dedicated to the rememberance of the most horiffic war in world history 
+
+        </p>
+        <p class="my-6 mx-16 text-lg">
+            all information and images are used in good faith 
+        </p>
+    </main>
+
 </template>
 
-<script>
-    export default {
+<script setup>
 
-    }
-    definePageMeta({  
-        layout: "custom"
-         });
+definePageMeta({
+    layout: "custom"
+});
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
 
 </style>
